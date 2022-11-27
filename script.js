3407b9884a8183a1a79305cd86517e71f46f0612
GuiSegatto <guisegatto23@gmail.com>
2022-11-27 15:54:27 -0300

    feat: add bold to the accordion header when click

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ const accordionItem = document.querySelectorAll(".accordionItem"); // Recebe tod
 
 
 accordionItem.forEach((item) => { // O item recebido será o accordion item clickado
-    const accordionHeaderItem = item.querySelector(".accordionHeader"); // Seleciona o accordionHeader (parte que será clickada)
-
+    const accordionHeaderItem = item.querySelector(".accordionHeader"); // Seleciona o accordionHeader (parte que foi clickada)
+    
     accordionHeaderItem.addEventListener("click", () => { // Ao clickar executa a arrow function
-        const accordionContentItem = item.querySelector(".accordionContent"); // Seleciona o content accordion (o que será exibido)
-
-        const itemActived =  document.querySelector(".active"); // Seleciona o elemento com classe .active
+        const accordionContentItem = item.querySelector(".accordionContent"); // Seleciona o content accordion (o que será exibido) do que foi selecionado
+        const itemActived =  document.querySelector(".active"); // Seleciona o elemento com classe .active quando foi clickado
 
         verifyActive(item, accordionContentItem, itemActived); // Executa a função que verifica se está ativo e passa os parametros
     });
@@ -21,8 +20,11 @@ contentActived = o que possuir classe active
 */
 
 function verifyActive(item, content, contentActived) { // Passa os parametros recebidos através do onclick acima
-    const iconItem = item.querySelector(".arrow");
-    const iconItemActived = document.querySelectorAll(".arrow"); 
+    const iconItem = item.querySelector(".arrow"); // Seleciona a img com classe "arrow" do item clickado
+    const iconItemActived = document.querySelectorAll(".arrow"); // Seleciona todas as imgs com tag "arrow"
+
+    const itemText = item.querySelector(".accordionTextHeader") // Seleciona o texto com a classe do elemento clickado
+    const textActived = document.querySelectorAll(".accordionTextHeader"); // Seleciona todas as classes
 
     /* 
         iconItem = seleciona o a img com classe .arrow clickada
@@ -31,6 +33,7 @@ function verifyActive(item, content, contentActived) { // Passa os parametros re
 
     // faz com que volte ao icon com a seta para baixo
     iconItemActived.forEach((item) => (item.classList.remove("arrowRotate")));
+    textActived.forEach((item) => (item.classList.remove("activeBold")));
 
     if (contentActived) {
         contentActived.style.height = 0; // Volta o height para 0
@@ -41,6 +44,7 @@ function verifyActive(item, content, contentActived) { // Passa os parametros re
         iconItem.classList.add("arrowRotate") // adiciona a seta para cima
         content.classList.add("active") // fecha o container
         content.style.height = content.scrollHeight + 10 + "px"; // scrollHeight faz com que caiba todo o content quando aberto
+        itemText.classList.add("activeBold")
     };
 };
 
